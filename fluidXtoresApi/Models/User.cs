d85159661f58aac934949a08683816516a03a390
Ronel Cruz <ronel.cruz.a8@gmail.com>
2022-04-07 22:35:25 +0200

    initial commit

---
+++ fluidXtoresApi/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace fluidXtoresApi.Models
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public long UserAge { get; set; }
+        
+
+    }
+}
---
+++ fluidXtoresApi/Models/userContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace fluidXtoresApi.Models
+{
+    public class userContext:DbContext
+    {
+        public userContext(DbContextOptions<userContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
